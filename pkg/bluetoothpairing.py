63b99790b1606d58f91c10c923e6b6a706570fdd
flatsiedatsie <unfold@gmail.com>
2026-04-15 22:54:28 +0200

    Add files via upload

--- pkg/bluetoothpairing.py
+++ pkg/bluetoothpairing.py
@@ -537,7 +537,7 @@ class BluetoothpairingAPIHandler(APIHandler):
                                     
                                     if line.startswith('Name:'):
                                         info_name = line.replace('Name:','').strip()
-                                        if len(info_name) > 3 and len(info_name) < 20:
+                                        if len(info_name) > 3 and len(info_name) < 20 and info_name.lower() != 'none':
                                             device['name'] = info_name
                                     
                                     if 'Icon: audio-card' in line:
